fee7c1bdfebcdd693a07640da8383a0a29da7dcc
mustak <mustakahmmed88@gmail.com>
2025-09-26 20:43:36 +0600

    nav bar added

---
+++ src/Route/Route.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+import Root from '../Pages/Root/Root';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Root,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+        
+    ]
+  },
+]);
